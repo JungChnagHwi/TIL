ebf6fdc0b41ad164c02d23729d5c5b6cfae3c65b
정창휘 <jch9937@gmail.com>
2023-09-26 17:33:08 +0900

    a

--- Django/연습용3/articles/views.py
+++ Django/연습용3/articles/views.py
@@ -76,3 +76,44 @@ def update(request, pk):
         'form': form,
     }
     return render(request, 'articles/update.html', context)
+
+
+
+
+
+
+
+
+
+# HW참고
+# from django.views.decorators.http import require_http_methods, require_POST,require_safe
+# from django.shortcuts import get_object_or_404
+
+
+# @require_POST()
+# def delete(request,pk):
+#     chatting = get_object_or_404(Chat,pk=pk)
+#     chatting.delete()
+#     return redirect("chattings:index")
+
+# @require_safe()
+# def detail(request,pk):
+#     chatting = get_object_or_404(Chat,pk=pk)
+#     context = {
+#         'chatting':chatting,
+#     }
+#     return render(request,'chattings/detail.html',context)
+
+# @require_http_methods(["GET", "POST"])
+# def create(request):
+#     if request.method == 'POST':
+#         form = ChatForm(request.POST,request.FILES)
+#         if form.is_valid:
+#             chatting = form.save()
+#             return redirect('chattings:detail',chatting.pk)
+#     else:
+#         form = ChatForm()
+#     context={
+#         'form':form
+#     }
+#     return render(request,'chattings/create.html',context)
