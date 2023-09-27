b894ff690592d2eb17e4439eb359cc9e5061ed0d
정창휘 <jch9937@gmail.com>
2023-09-27 10:53:43 +0900

    a

--- Django/08-django-static/articles/models.py
+++ Django/08-django-static/articles/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
+    # image = models.ImageField(blank=True, upload_to ='images/') # media 안에 상세경로 설정
+    image = models.ImageField(blank=True, upload_to ='%Y/%m/%d/') # media 안에 날짜명으로 세부경로 생성
+    # False면 모든 게시글 쓸 때마다 넣어야 함, 공백 허용은 안 넣어도 상관없다.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
