0ad88aea1133fcea232faae0507cf2a41529476b
정창휘 <jch9937@gmail.com>
2023-10-18 16:00:36 +0900

    a

--- sql/03-02-many-to-many-relationships/accounts/models.py
+++ sql/03-02-many-to-many-relationships/accounts/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    #symmetrical=False -> 영희가 철수를 팔로우해도 철수가 영희를 팔로우되지 않음
