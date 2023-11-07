257dda7cbfe977a079bab41762795df80b61e523
정창휘 <jch9937@gmail.com>
2023-11-07 13:38:00 +0900

    a

---
+++ Vue/04-single-file-component/vue-project/src/components/MyComponenet.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>MyComponent</h2>
+    <MycomponentItem />
+    <MycomponentItem />
+    <MycomponentItem />
+  </div>
+</template>
+
+
+<script setup>
+import MycomponentItem from '@/components/MyComponentItem.vue'
+</script>
